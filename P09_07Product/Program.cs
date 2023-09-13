0df9af8c6170b08b00cb16c8615420cae30f9451
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-09-13 13:39:12 +0200

    Completed P09_07Product

---
+++ P09_07Product/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ok ok uhm uhm now like uhm input two numbies and i will calculate their product11!!!!!!1!!!!");
+
+Console.WriteLine("Ok first one!!!");
+int userInput1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ok second one!!!");
+int userInput2 = int.Parse(Console.ReadLine());
+
+int answer = userInput1 * userInput2;
+
+Console.WriteLine($"Uhm uhm the answer is {answer}!!!");
