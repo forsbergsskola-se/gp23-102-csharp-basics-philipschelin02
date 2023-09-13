32917bfa46b343ff5e0e7e4ecfb318cfde74ac22
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-09-13 15:46:45 +0200

    Completed P09_09Hypotenuse

--- P09_09Hypotenuse/Program.cs
+++ P09_09Hypotenuse/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Ok ok uhm uhm now like uhm input two numbies and i will calculate their hypotenuse11!!!!!!1!!!!");
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ok first side!!!");
+int userInput1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ok second side!!!");
+int userInput2 = int.Parse(Console.ReadLine());
+
+double answer = Math.Sqrt(userInput1 * userInput1 + userInput2 * userInput2);
+
+Console.WriteLine($"Uhm uhm the answer is {answer}!!!");
