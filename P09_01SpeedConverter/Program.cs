04ece53409e662ae39a8d57b1086ae4969c3d978
Philip Schelin <philip.schelin@student.forsbergsskola.se>
2023-09-13 11:48:42 +0200

    yoooo i did it

---
+++ P09_01SpeedConverter/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Pls gib km/h input and i'll convert to m/s!!!!");
+
+string input = Console.ReadLine();
+float numberInput = float.Parse(input);
+float i = numberInput * 1000 / 60 / 60;
+Console.WriteLine($"omg thats {i} m/s!!!");
